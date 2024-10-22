9dcc019a4e0537ef852702438de9e7d380a79810
Ing-Javier-Sistem <145406699+Ing-Javier-Sistem@users.noreply.github.com>
2024-10-22 16:56:50 -0500

    update file orm

--- BackEnd/Apps/Orm/orm.py
+++ BackEnd/Apps/Orm/orm.py
@@ -8,6 +8,10 @@ from BackEnd.Apps.Orm.models import *
 # from BackEnd.Apps.Orm.orm import Orm
 # orm_instance = Orm()
 
+# Comandos
+# python manage.py shell
+# python manage.py shell_plus --print-sql      USAR ESTE DE AQUI
+
 
 # Practica JavicSoftCode
 class Orm:
@@ -102,3 +106,31 @@ class Orm:
       if connection.queries:
         last_query = connection.queries[-1]['sql']
         print(f"\nConsulta SQL ejecutada para '{tipo_a_buscar}':\n{last_query}")
+
+
+
+# # Practica Julissa
+# # Insertar un registro directamente en la base de datos
+# tipo1 = TipoSangre.objects.create(tipo="X+", descripcion="Tipo X positivo")
+#
+# # Terminal Shell Plus >>>>>
+# # In [1]: tipo1 = TipoSangre.objects.create(tipo="X+", descripcion="Tipo X positivo")
+#
+# # CONSULTA SQL <<<<<
+# # INSERT INTO "Orm_tiposangre" ("tipo", "descripcion")
+# # VALUES ('X+', 'Tipo X positivo') RETURNING "Orm_tiposangre"."id"
+#
+# # =========================================================================================
+#
+# # Crear el registro en memoria (sin guardarlo aún en la base de datos)
+# tipo2 = TipoSangre(tipo="V-", descripcion="Tipo V negativo")
+# # Guardar el registro en la base de datos
+# tipo2.save()
+#
+# # Terminal Shell Plus >>>>>
+# # In [2]: tipo2 = TipoSangre(tipo="V-", descripcion="Tipo V negativo")
+# #   ...: tipo2.save()
+#
+# # CONSULTA SQL <<<<<
+# # INSERT INTO "Orm_tiposangre" ("tipo", "descripcion")
+# # VALUES ('V-', 'Tipo V negativo') RETURNING "Orm_tiposangre"."id"
